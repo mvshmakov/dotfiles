d22e0ac3b520f61b075eac348073aa22382d4fb8
Maxim Shmakov <mvshmakov@mail.ru>
2022-10-26 15:51:18 +0400

    conf(nvim): replace deprecated update_capabilities with default_capabilities

--- nvim/.config/nvim/lua/mvshmakov/lsp/handlers.lua
+++ nvim/.config/nvim/lua/mvshmakov/lsp/handlers.lua
@@ -77,7 +77,8 @@ M.on_attach = function(client, bufnr)
 
     M.capabilities = vim.lsp.protocol.make_client_capabilities()
     M.capabilities.textDocument.completion.completionItem.snippetSupport = true
-    M.capabilities = cmp_nvim_lsp.update_capabilities(M.capabilities)
+    M.capabilities = cmp_nvim_lsp.default_capabilities(M.capabilities)
+
 
     lsp_keymaps(bufnr)
     -- TODO: decide if I need illuminate
