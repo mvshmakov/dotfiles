a3acfbc11feb93efb6e79234f523466ae84b9305
Maxim Shmakov <mvshmakov@mail.ru>
2023-03-15 15:33:29 +0400

    feat(lsyncd): update config according to docs

--- lsyncd/.config/lsyncd/lsyncd.lua
+++ lsyncd/.config/lsyncd/lsyncd.lua
@@ -1,38 +1,40 @@
 home = "/Users/mvshmakov"
 remote_host = "pi@mvshmakov-pi.local"
 
-settings {
-    logfile = home.."/.local/share/lsyncd/lsyncd.log",          --Log path
-    statusFile = home.."/.local/state/lsyncd/lsyncd.status",    --Status file
-    pidfile = home.."/.local/state/lsyncd/lsyncd.pid",          --pid File path
-    nodaemon = true,                                            --daemon Function
-    insist=true,
-    statusInterval = 1,                                         --Minimum write time of status file
-    maxProcesses = 1,                                           --Maximum process
-    maxDelays = 1,                                              --Maximum delay
-}
+-- https://lsyncd.github.io/lsyncd/manual/config/file/
+settings({
+    logfile = home .. "/.local/share/lsyncd/lsyncd.log",
+    statusFile = home .. "/.local/state/lsyncd/lsyncd.status",
+    pidfile = home .. "/.local/state/lsyncd/lsyncd.pid",
 
+    nodaemon = true, -- do not detach from tty on startup
+    insist = true, -- keep running at startup although one or more targets failed due to not being reachable.
+    statusInterval = 1, -- minimum write time of status file
+    maxProcesses = 1, -- maximum process
+    maxDelays = 1, -- maximum delay
+})
+
+-- Can add arbutrary number of sources
 sourceList = {}
-sourceList[home..'/projects/work/iterative/studio'] = '/home/pi/projects/work/iterative/studio'
+sourceList[home .. "/projects/work/iterative/studio"] = "/home/pi/projects/work/iterative/studio"
 
 for from_source, target_source in pairs(sourceList) do
-    sync {
-        default.rsync,
-        source = from_source,
+    sync({
+        -- Should be one of 'direct' | 'rsync' | 'rsyncssh'
+        -- 'direct' uses /bin/cp or /bin/mv commands during operation to optimize performance,
+        -- though startup is still done by rsync
+        -- https://lsyncd.github.io/lsyncd/manual/config/layer4/#defaultdirect
+        default.direct,
         delete = true,
-        target = remote_host..":"..target_source,
-        exclude = {".vscode/*", ".git/*", "node_modules/*", "__pycache__/*", ".DS_Store"},
-        rsync     = {
-            binary = "/usr/local/bin/rsync",
-            -- binary ="/usr/bin/rsync",
+        source = from_source,
+        target = remote_host .. ":" .. target_source,
+        exclude = { ".vscode/*", ".git/*", "node_modules/*", "__pycache__/*", ".history/*", ".DS_Store" },
+        rsync = {
+            -- https://stackoverflow.com/questions/13713101/rsync-exclude-according-to-gitignore-hgignore-svnignore-like-filter-c#comment43645099_15373763
+            binary = "/usr/bin/env rsync --exclude='/.git' --filter='dir-merge,- .gitignore'",
             archive = true,
             compress = true,
-            bwlimit   = 2000,
-            -- TODO: use SSH_KEY_PATH
-            rsh = "ssh -i"..home.."/.ssh/id_rsa"
-            -- rsh = "/usr/bin/ssh -p 22 -o StrictHostKeyChecking=no"
-            -- If you want to specify another port, use the rsh
-        }
-    }
+            bwlimit = 2000,
+        },
+    })
 end
-
