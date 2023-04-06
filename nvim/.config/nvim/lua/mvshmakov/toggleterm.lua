c8691728237376113527ffe8987ccdbb8f9d42a8
Maxim Shmakov <mvshmakov@mail.ru>
2023-04-06 22:29:35 +0400

    fix(nvim): change lazygit keymap

--- nvim/.config/nvim/lua/mvshmakov/toggleterm.lua
+++ nvim/.config/nvim/lua/mvshmakov/toggleterm.lua
@@ -45,7 +45,7 @@ function _G._lazygit_toggle()
     lazygit:toggle()
 end
 
-vim.api.nvim_set_keymap("n", "<leader>g", "<cmd>lua _lazygit_toggle()<CR>", {
+vim.api.nvim_set_keymap("n", "<leader>lg", "<cmd>lua _lazygit_toggle()<CR>", {
     noremap = true,
     silent = true,
 })
