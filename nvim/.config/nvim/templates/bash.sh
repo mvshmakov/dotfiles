feae875a670659e9f929679c54b3624cd19e72fc
Maxim Shmakov <mvshmakov@mail.ru>
2023-04-06 22:31:36 +0400

    feat(nvim): update POSIX/Bash shell templates

--- nvim/.config/nvim/templates/bash.sh
+++ nvim/.config/nvim/templates/bash.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 #
-# Description
+# {Description comment}
 
+# A non-verbose  alternative to `set -o errexit -o nounset`
+# Exits from the script on error (errexit) and treat unset variables as an error (nounset)
+# Pipefail will fail the whole pipeline if any of the parts have failed
+# `set -x` can be added to print lines before their execution - useful for debugging
+# All options: https://www.gnu.org/software/bash/manual/html_node/The-Set-Builtin.html
 # Unofficial Bash strict mode http://redsymbol.net/articles/unofficial-bash-strict-mode/
 set -euo pipefail
+# Internal Field Separator (IFS) - splits arrays on this sequence of characters
+# http://redsymbol.net/articles/unofficial-bash-strict-mode/
 IFS=$'\n\t'
--- nvim/.config/nvim/templates/shell.sh
+++ nvim/.config/nvim/templates/shell.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env sh
 #
-# Description
+# {Description comment}
+
+# A non-verbose  alternative to `set -o errexit -o nounset`
+# Exits from the script on error (errexit) and treat unset variables as an error (nounset)
+# `set -x` can be added to print lines before their execution - useful for debugging
+# All options: https://www.gnu.org/software/bash/manual/html_node/The-Set-Builtin.html
+# Unofficial POSIX strict mode: https://gist.github.com/EvgenyOrekhov/5c1418f4710558b5d6717d9e69c6e929
+set -eu
+# Internal Field Separator (IFS) - splits arrays on this sequence of characters
+# http://redsymbol.net/articles/unofficial-bash-strict-mode/
+IFS=$(printf '\n\t')
